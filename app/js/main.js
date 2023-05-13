f20336a183dbdea90a1b857b936fe6ed7bf6d143
AlexIstra1989 <alex888fcsm@gmail.com>
2023-05-13 10:48:42 +0300

    added section main-block  html

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,41 @@ $('.header__btn').on('click', function() {
  $('.menu').toggleClass('menu_active'),
  $('.header__btn').toggleClass('header__btn_active');
 });
+
+
+//Slider 
+new Swiper ('.main-block__slider', {
+ slidesPerView: 1,
+ autoHeight: true,
+ spaceBetween: 0,
+ loop: true,
+
+ pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+ },
+
+ // Брейкпоинты
+		
+			// breakpoints: {
+   //  320: {
+			// 		slidesPerView: 1,
+			// 		spaceBetween: 10,
+			// 		autoHeight: true,
+			// 	},
+			// 	490: {
+			// 		slidesPerView: 2,
+			// 		spaceBetween: 20,
+			// 		autoHeight: true,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 2.4,
+			// 		spaceBetween: 20,
+			// 	},
+			// 	991: {
+			// 		slidesPerView: 3.5,
+			// 		spaceBetween: 30,
+			// 	},
+			// },
+			
+});
