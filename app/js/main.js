3ffc80643bdcadfc4741df33f0c8649780f30350
AlexIstra1989 <alex888fcsm@gmail.com>
2023-05-18 16:26:53 +0300

    added testimonials section

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,6 @@ $('.header__btn').on('click', function() {
  $('.header__btn').toggleClass('header__btn_active');
 });
 
-
 //Slider 
 new Swiper ('.main-block__slider', {
  slidesPerView: 1,
@@ -15,29 +14,17 @@ new Swiper ('.main-block__slider', {
  pagination: {
   el: '.swiper-pagination',
   clickable: true,
- },
+ },			
+});
 
- // Брейкпоинты
-		
-			// breakpoints: {
-   //  320: {
-			// 		slidesPerView: 1,
-			// 		spaceBetween: 10,
-			// 		autoHeight: true,
-			// 	},
-			// 	490: {
-			// 		slidesPerView: 2,
-			// 		spaceBetween: 20,
-			// 		autoHeight: true,
-			// 	},
-			// 	768: {
-			// 		slidesPerView: 2.4,
-			// 		spaceBetween: 20,
-			// 	},
-			// 	991: {
-			// 		slidesPerView: 3.5,
-			// 		spaceBetween: 30,
-			// 	},
-			// },
-			
+new Swiper ('.testimonials__slider', {
+ slidesPerView: 1,
+ autoHeight: true,
+ spaceBetween: 0,
+ loop: true,
+
+ pagination: {
+  el: '.testimonials__pagination',
+  clickable: true,
+ },			
 });
